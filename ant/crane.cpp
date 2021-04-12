1414c3259a6a58b61119ee3b5449e9d1e815b577
rmasutani <masutanirugby@gmail.com>
2021-04-12 13:53:13 +0900

    Add crane

---
+++ ant/crane.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define rep(i, n) for (int i = 0; i < (n); i++) 
+using namespace std;
+const int NMAX = 10010;
+
+int L[NMAX];
+int main() {
+    int N;
+    cin >> N;
+    rep(i, N) cin >> L[i];
+    
+}
